34af820429b91e65911eb476c26c274e4c9b610b
PhamSang1210 <phamhongsang12x10@gmail.com>
2024-09-30 10:16:14 +0700

    Config&&Anotation

--- src/main/java/com/javaweb/repository/entity/DistrictEntity.java
+++ src/main/java/com/javaweb/repository/entity/DistrictEntity.java
@@ -1,9 +1,24 @@
 package com.javaweb.repository.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "district")
 public class DistrictEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "code")
 	private String code;
-	private String name;
+	
+	@Column(name = "name")
+	private String name; 
 
 	public Long getId() {
 		return id;
